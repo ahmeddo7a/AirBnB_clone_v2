fe0c8130ea5638661a1325bb4f363cb6a0e291d0
ahmeddo7a <ahmedholdo@gmail.com>
2023-09-06 16:14:37 -0700

    fix setup

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -31,11 +31,10 @@ SERVER_CONFIG="server {
 HOME_PAGE="<!DOCTYPE html>
 <html lang='en-US'>
 	<head>
-		<title>Home - AirBnB Clone</title>
-	</head>
-	<body>
-		<h1>Welcome to AirBnB!</h1>
-	<body>
+  </head>
+  <body>
+    Holberton School
+  </body>
 </html>
 "
 # shellcheck disable=SC2230
